e5576f55bb2b5ef0cdf57f4b1f9a46a2ce989145
Robson <robhenrod@gmail.com>
2020-08-06 11:32:43 -0300

    Day 3: A escolha da stack
    Conectando a API com o Front-end Web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,32 +2,55 @@ import React from 'react';
 
 import whatsappIcon from  '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+interface TeacherItemProps {
+    teacher: {
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;        
+        name: string;
+        subject: string;
+        whatsapp: string;
+    };
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/9096404?s=460&v=4" alt="Robson"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Robson H. Rodrigues</strong>
-                    <span>Lorem Ipsum</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae ipsum laoreet, semper tortor eget, vulputate neque. Aliquam pellentesque libero eget condimentum finibus. Maecenas auctor tellus in ornare finibus. Duis pretium lectus quis dignissim dignissim. Pellentesque iaculis urna ut nunc interdum bibendum. Nam congue efficitur odio, et ultricies augue euismod eu. Nam rhoncus viverra consequat. Phasellus nisl ligula, consectetur ut ipsum vel, accumsan rhoncus eros. Duis et quam eu lorem elementum suscipit. Vestibulum lobortis a dolor vitae euismod. Ut quis lobortis ante. Aenean a tellus tincidunt, molestie ipsum eget, tincidunt sem. Curabitur pellentesque dignissim tortor, at varius neque. Fusce accumsan felis ut quam scelerisque sagittis.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 20,00</strong>
+                    <strong>{teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    rel="noopener noreferrer" 
+                    target="_blank" 
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
